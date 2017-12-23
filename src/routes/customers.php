b3516fe1a361838938ca87fbfb89bddaf14b7851
Nejc Ribic <nejc.ribic95@gmail.com>
2017-12-23 17:54:38 +0100

    Initial commit

---
+++ src/routes/customers.php
@@ -0,0 +1,11 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+
+$app = new \Slim\App;
+
+// Get All Customers
+$app->get('/api/customers', function (Request $request, Response $response){
+    echo 'CUSTOMERS';
+});
